4a5f364e680dd9ec2df7c846788b3ea388781f73
ltroullier01 <lael.troullier@orange.fr>
2026-01-13 18:30:31 +0100

    Dossier_Modelesiation finale 13/01

---
+++ Modélisation_Finale_VALIDE_13_01_2026/03_BF_Courant/donnee_moteur_seul_06_11_2025.m
@@ -0,0 +1,15 @@
+% données pour 1 moteur
+
+R= 1.52; % résistance de l'induit
+Rch= 10; % charge
+
+L=2.2e-3; % inductance propre de l'induit
+J=8.3e-5; % moment d'inertie pour une machine
+U=48; % tension d'alimentation
+Kphi=0.127; % constante de couple, de force électromotrice
+
+f=(0.53*30*10^-2)/(1000*pi)  %frottement
+C0 = 2.4*10^-2              %couple de frottement sec du moteur
+
+UM=24
+u_commande=7.5
---
+++ Modélisation_Finale_VALIDE_13_01_2026/08_Test_&_Comparaison_Maquette/Tachy/Comparaison Simu/Comparaison_maquette_simu_Tachy.m
@@ -0,0 +1,73 @@
+%Avant d'éxécuter ce code, faire attention a bien éxécuter la simulation
+%Simulink pour avoir les valeur matlab
+
+%Lecture des fichiers de points
+data_maquette = readmatrix(['Scope_Tachy_Charge.csv'], 'NumHeaderLines', 2);
+data_psim = readmatrix('Data_Essai_PSIM_Tachy.txt');
+
+% Extraction des colonnes de temps
+t = data_maquette(:, 1); 
+t = t+1;
+t_psim = data_psim(:, 1);
+% Extraction des grandeurs mesurées
+Consigne_maquette = data_maquette(:, 2);
+Vitesse_maquette = data_maquette(:, 3);
+Courant_maquette = data_maquette(:, 4);
+% Extraction des grandeurs simulées sur PSIM
+Consigne_psim = data_psim(:, 2);
+Vitesse_psim = data_psim(:, 3);
+Courant_psim = data_psim(:, 4);
+
+%Synchronisation des signaux
+offset = 0.88; 
+t_synchro = t + offset;
+
+%Ajout du facteur 6/1000 pour la vitesse venant de la dynamo tachymétrique
+Vitesse_maquette = Vitesse_maquette*(1000/6);
+
+%On filtre les données pour ne prendre que quelques périodes
+filtre = t_synchro <= 3;
+t_maquette_coupe = t_synchro(filtre);
+Consigne_maquette_coupe = Consigne_maquette(filtre);
+Vitesse_maquette_coupe = Vitesse_maquette(filtre);
+Courant_maquette_coupe = Courant_maquette(filtre);
+
+
+%Tracé des courbes
+close all;
+
+%Tracé de la consigne
+figure;
+plot(t_maquette_coupe, Consigne_maquette_coupe, 'r', 'LineWidth', 1.5); hold on;
+plot(t_psim, Consigne_psim, 'b', 'LineWidth', 1.5);
+plot(out.consigne, 'g', 'LineWidth', 1.5);
+
+grid on;
+xlabel('Temps (s)');
+ylabel('Tension (V)');
+title('Comparaison Consigne');
+legend('Maquette', 'PSIM', 'Matlab');
+
+%Tracé de la vitesse
+figure;
+plot(t_maquette_coupe, Vitesse_maquette_coupe, 'r', 'LineWidth', 1.5); hold on;
+plot(t_psim, Vitesse_psim, 'b', 'LineWidth', 1.5);
+plot(out.vitesse, 'g', 'LineWidth', 1.5);
+
+grid on;
+xlabel('Temps (s)');
+ylabel('Vitesse (tr/min)');
+title('Comparaison Vitesse');
+legend('Maquette', 'PSIM', 'Matlab');
+
+%Tracé du courant moteur
+figure;
+plot(t_maquette_coupe, Courant_maquette_coupe, 'r', 'LineWidth', 1.5); hold on;
+plot(t_psim, Courant_psim, 'b', 'LineWidth', 1.5);
+plot(out.courant_moteur, 'g', 'LineWidth', 1.5);
+
+grid on;
+xlabel('Temps (s)');
+ylabel('Courant Moteur (A)');
+title('Comparaison Courant');
+legend('Maquette', 'PSIM', 'Matlab');
